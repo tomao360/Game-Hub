3f0716fcaa65cd106fac47bf777918597b4d5899
tomao360 <tomao360@gmail.com>
2023-05-04 15:35:01 +0300

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,11 +15,21 @@ export interface Game {
   metacritic: number;
 }
 
-// The useGames hook passes the selectedGenre as a query string parameter to the useData hook
+// The useGames hook passes the selectedGenre and the selectedPlatform as a query string parameter to the useData hook - params object we sending to the server
 // We also pass an array of dependencies, so if any of the dependencies changes, our effect will rerun and refresh the data from the server
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
-    selectedGenre?.id,
-  ]); // The params is one of the properties of AxiosRequestConfig object
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        parent_platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  ); // The params is one of the properties of AxiosRequestConfig object
 
 export default useGames;
