0a259db809519862d60b0b982ac0ae0acb6862ac
tomao360 <tomao360@gmail.com>
2023-05-07 13:23:30 +0300

    Refactoring - fetch games with React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -20,17 +21,20 @@ export interface Game {
 // The useGames hook passes the gameQuuery as a query string parameter to the useData hook - params object we sending to the server
 // We also pass an array of dependencies, so if any of the dependencies changes, our effect will rerun and refresh the data from the server
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  ); // The params is one of the properties of AxiosRequestConfig object
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          // The params is one of the properties of AxiosRequestConfig object
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
