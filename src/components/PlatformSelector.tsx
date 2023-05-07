92e05b83bb0283c0cf3c4a01d5958867a257026f
tomao360 <tomao360@gmail.com>
2023-05-07 12:28:48 +0300

    Fetching Genres and Fetching Platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -22,7 +22,7 @@ const PlatformSelector = ({
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
   id: number;
@@ -6,6 +9,15 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    // When we provide initialData, this data inserted into the cache, because we gave our data a stale time of 24h,
+    // for 24h our data is going to be fresh (no request will be made to the backend to fetch the genres)
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default useGenres;
