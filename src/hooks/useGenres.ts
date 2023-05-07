994e1d2f7e1b27487b63bdaa1a259a3841f9f9d2
tomao360 <tomao360@gmail.com>
2023-05-07 16:15:47 +0300

    Refactor - simplify time calculations (npm ms)

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
 
@@ -14,7 +15,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"),
     // When we provide initialData, this data inserted into the cache, because we gave our data a stale time of 24h,
     // for 24h our data is going to be fresh (no request will be made to the backend to fetch the genres)
     initialData: genres,
